83c89861d3dd32bfd56f01d2fbcc83715c2a9b71
natgoutzel <natgoutzel@hotmail.com>
2016-01-09 21:35:01 +0200

    Create JSONParser for server communication

---
+++ android/app/src/main/java/com/dreamteam/snapichat/utils/JSONParser.java
@@ -0,0 +1,91 @@
+package com.dreamteam.snapichat.utils;
+
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    public JSONParser() {
+
+    }
+
+    public JSONObject makeHttpRequest(String urlStr, String method, List<NameValuePair> params) {
+        // Making HTTP request
+        try {
+            URL url = new URL(urlStr + getQuery(params));
+
+            Log.d("URL Request", url.toString());
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method);
+            conn.connect();
+
+            Log.d("Response Code: ", Integer.toString(conn.getResponseCode()));
+
+            is = new BufferedInputStream(conn.getInputStream());
+        } catch (IOException e) {
+            Log.e("HTTP Request", e.toString());
+        }
+
+        // Getting JSON Response
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+    }
+
+    private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (NameValuePair pair : params)
+        {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+}
